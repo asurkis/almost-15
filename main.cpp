235198419c05f68a7d0f9352054fffc5fdb320ab
Anton Surkis <antonsurkis@gmail.com>
2022-01-28 11:22:09 +0300

    Add highlight of selected cell

--- main.cpp
+++ main.cpp
@@ -4,20 +4,29 @@
 using namespace std;
 
 const int SCREEN_SIZE = 900;
-const int CELL_SIZE = 160;
-const int BORDER_WIDTH = 6;
-const int CELL_OFFSET = CELL_SIZE + BORDER_WIDTH;
-const int BOARD_SIZE = 4 * CELL_OFFSET + BORDER_WIDTH;
+const int CELL_SIZE_HALF = 80;
+const int CELL_SIZE = 2 * CELL_SIZE_HALF;
+const int CELL_MARGIN = 3;
+const int BORDER_WIDTH = 2 * CELL_MARGIN;
+const int CELL_OFFSET = CELL_SIZE + 2 * CELL_MARGIN;
+const int BOARD_SIZE = 4 * CELL_OFFSET;
 const int BOARD_START = (SCREEN_SIZE - BOARD_SIZE) / 2;
+const int BOARD_OUTER_SIZE = BOARD_SIZE + 2 * CELL_MARGIN;
+const int BOARD_OUTER_START = BOARD_START - CELL_MARGIN;
 const int CELL_FONT_SIZE = 40;
 const Color BOARD_BACKGROUND_COLOR = {16, 16, 16, 255};
 const Color CELL_TEXT_COLOR = BOARD_BACKGROUND_COLOR;
+const Color HIGHLIGHT_COLOR = {0, 0, 255, 64};
 
 struct GameState {
   int empty_x;
   int empty_y;
   int board[4][4];
 };
+
+int cell_to_screen(int coord) { return BOARD_START + CELL_OFFSET * coord; }
+
+int screen_to_cell(int coord) { return (coord - BOARD_START) / CELL_OFFSET; }
 
 int main() {
   InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Almost 15 Game");
@@ -33,27 +42,44 @@ int main() {
   state.board[state.empty_y][state.empty_x] = 0;
 
   while (!WindowShouldClose()) {
+    int mouse_x = GetMouseX();
+    int mouse_y = GetMouseY();
+    int selected_cell_x = -1;
+    int selected_cell_y = -1;
+
+    bool mouse_over_cell =
+        BOARD_START <= mouse_x && mouse_x < BOARD_START + BOARD_SIZE &&
+        BOARD_START <= mouse_y && mouse_y < BOARD_START + BOARD_SIZE;
+    if (mouse_over_cell) {
+      selected_cell_x = screen_to_cell(mouse_x);
+      selected_cell_y = screen_to_cell(mouse_y);
+    }
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawRectangle(BOARD_START, BOARD_START, BOARD_SIZE, BOARD_SIZE,
-                  BOARD_BACKGROUND_COLOR);
+    DrawRectangle(BOARD_OUTER_START, BOARD_OUTER_START, BOARD_OUTER_SIZE,
+                  BOARD_OUTER_SIZE, BOARD_BACKGROUND_COLOR);
     for (int i = 0; i < 4; ++i) {
       for (int j = 0; j < 4; ++j) {
         if (i == state.empty_y && j == state.empty_x) {
           continue;
         }
-        DrawRectangle(BOARD_START + CELL_OFFSET * j + BORDER_WIDTH,
-                      BOARD_START + CELL_OFFSET * i + BORDER_WIDTH, CELL_SIZE,
-                      CELL_SIZE, RAYWHITE);
+        DrawRectangle(cell_to_screen(j) + CELL_MARGIN,
+                      cell_to_screen(i) + CELL_MARGIN, CELL_SIZE, CELL_SIZE,
+                      RAYWHITE);
         const char *text = TextFormat("%d", state.board[i][j]);
         int width = MeasureText(text, CELL_FONT_SIZE);
-        DrawText(text,
-                 BOARD_START + CELL_OFFSET * j + BORDER_WIDTH +
-                     (CELL_SIZE - width) / 2,
-                 BOARD_START + CELL_OFFSET * i + BORDER_WIDTH +
-                     (CELL_SIZE - CELL_FONT_SIZE) / 2,
-                 CELL_FONT_SIZE, CELL_TEXT_COLOR);
+        DrawText(
+            text, cell_to_screen(j) + CELL_MARGIN + (CELL_SIZE - width) / 2,
+            cell_to_screen(i) + CELL_MARGIN + (CELL_SIZE - CELL_FONT_SIZE) / 2,
+            CELL_FONT_SIZE, CELL_TEXT_COLOR);
       }
+    }
+
+    if (mouse_over_cell) {
+      DrawRectangle(cell_to_screen(selected_cell_x),
+                    cell_to_screen(selected_cell_y), CELL_OFFSET, CELL_OFFSET,
+                    HIGHLIGHT_COLOR);
     }
     EndDrawing();
   }
