6109ecdbe13d39959e3bd3239e096589f562b470
Anton Surkis <antonsurkis@gmail.com>
2022-01-28 12:16:00 +0300

    Add shuffle

--- main.cpp
+++ main.cpp
@@ -3,43 +3,80 @@
 
 using namespace std;
 
-const int SCREEN_SIZE = 900;
-const int CELL_SIZE_HALF = 80;
-const int CELL_SIZE = 2 * CELL_SIZE_HALF;
-const int CELL_MARGIN = 3;
-const int BORDER_WIDTH = 2 * CELL_MARGIN;
-const int CELL_OFFSET = CELL_SIZE + 2 * CELL_MARGIN;
-const int BOARD_SIZE = 4 * CELL_OFFSET;
-const int BOARD_START = (SCREEN_SIZE - BOARD_SIZE) / 2;
-const int BOARD_OUTER_SIZE = BOARD_SIZE + 2 * CELL_MARGIN;
-const int BOARD_OUTER_START = BOARD_START - CELL_MARGIN;
-const int CELL_FONT_SIZE = 40;
-const Color BOARD_BACKGROUND_COLOR = {16, 16, 16, 255};
-const Color CELL_TEXT_COLOR = BOARD_BACKGROUND_COLOR;
-const Color HIGHLIGHT_COLOR = {0, 0, 255, 64};
+static const int SCREEN_SIZE = 900;
+static const int CELL_SIZE_HALF = 80;
+static const int CELL_SIZE = 2 * CELL_SIZE_HALF;
+static const int CELL_MARGIN = 3;
+static const int BORDER_WIDTH = 2 * CELL_MARGIN;
+static const int CELL_OFFSET = CELL_SIZE + 2 * CELL_MARGIN;
+static const int BOARD_SIZE = 4 * CELL_OFFSET;
+static const int BOARD_START = (SCREEN_SIZE - BOARD_SIZE) / 2;
+static const int BOARD_OUTER_SIZE = BOARD_SIZE + 2 * CELL_MARGIN;
+static const int BOARD_OUTER_START = BOARD_START - CELL_MARGIN;
+static const int CELL_FONT_SIZE = 40;
+static const Color BOARD_BACKGROUND_COLOR = {16, 16, 16, 255};
+static const Color CELL_TEXT_COLOR = BOARD_BACKGROUND_COLOR;
+static const Color HIGHLIGHT_COLOR = {0, 0, 255, 64};
 
-struct GameState {
+struct Game {
   int empty_x;
   int empty_y;
   int board[4][4];
+
+  Game() {
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        board[i][j] = 4 * i + j + 1;
+      }
+    }
+    empty_x = 3;
+    empty_y = 3;
+    board[empty_y][empty_x] = 0;
+  }
+
+  void shuffle(int steps) {
+    for (int i = 0; i < steps; ++i) {
+      int move = GetRandomValue(0, 3);
+      int dx = 0, dy = 0;
+      (move > 1 ? dx : dy) = 2 * (move % 2) - 1;
+      int x = empty_x + dx;
+      int y = empty_y + dy;
+      if (x < 0 || y < 0 || x > 3 || y > 3) {
+        x = empty_x - dx;
+        y = empty_y - dy;
+      }
+      printf("move=%d, dx=%d, dy=%d, x=%d y=%d\n", move, dx, dy, x, y);
+      make_move(x, y);
+    }
+  }
+
+  bool make_move(int x, int y) {
+    int dx = x - empty_x;
+    int dy = y - empty_y;
+    if (abs(dx) + abs(dy) != 1) {
+      return false;
+    }
+
+    swap(board[y][x], board[empty_y][empty_x]);
+    empty_x = x;
+    empty_y = y;
+    return true;
+  }
 };
 
-int cell_to_screen(int coord) { return BOARD_START + CELL_OFFSET * coord; }
+static int cell_to_screen(int coord) {
+  return BOARD_START + CELL_OFFSET * coord;
+}
 
-int screen_to_cell(int coord) { return (coord - BOARD_START) / CELL_OFFSET; }
+static int screen_to_cell(int coord) {
+  return (coord - BOARD_START) / CELL_OFFSET;
+}
 
 int main() {
   InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Almost 15 Game");
 
-  struct GameState state;
-  for (int i = 0; i < 4; ++i) {
-    for (int j = 0; j < 4; ++j) {
-      state.board[i][j] = 4 * i + j + 1;
-    }
-  }
-  state.empty_x = 3;
-  state.empty_y = 3;
-  state.board[state.empty_y][state.empty_x] = 0;
+  struct Game game;
+  game.shuffle(10000);
 
   while (!WindowShouldClose()) {
     int mouse_x = GetMouseX();
@@ -57,14 +94,7 @@ int main() {
 
     bool is_mouse_pressed = IsMouseButtonPressed(MOUSE_BUTTON_LEFT);
     if (is_mouse_pressed && mouse_over_cell) {
-      int dx = selected_cell_x - state.empty_x;
-      int dy = selected_cell_y - state.empty_y;
-      if (abs(dx) + abs(dy) == 1) {
-        swap(state.board[selected_cell_y][selected_cell_x],
-             state.board[state.empty_y][state.empty_x]);
-        state.empty_x = selected_cell_x;
-        state.empty_y = selected_cell_y;
-      }
+      game.make_move(selected_cell_x, selected_cell_y);
     }
 
     BeginDrawing();
@@ -73,13 +103,13 @@ int main() {
                   BOARD_OUTER_SIZE, BOARD_BACKGROUND_COLOR);
     for (int i = 0; i < 4; ++i) {
       for (int j = 0; j < 4; ++j) {
-        if (i == state.empty_y && j == state.empty_x) {
+        if (i == game.empty_y && j == game.empty_x) {
           continue;
         }
         DrawRectangle(cell_to_screen(j) + CELL_MARGIN,
                       cell_to_screen(i) + CELL_MARGIN, CELL_SIZE, CELL_SIZE,
                       RAYWHITE);
-        const char *text = TextFormat("%d", state.board[i][j]);
+        const char *text = TextFormat("%d", game.board[i][j]);
         int width = MeasureText(text, CELL_FONT_SIZE);
         DrawText(
             text, cell_to_screen(j) + CELL_MARGIN + (CELL_SIZE - width) / 2,
@@ -95,4 +125,7 @@ int main() {
     }
     EndDrawing();
   }
+
+  CloseWindow();
+  return 0;
 }
